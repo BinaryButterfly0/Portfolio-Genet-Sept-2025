be189ef5e0a1570d14dc32bb2d0ef6dbc8fde1fd
BinaryButterfly0 <genetshibru0@gmail.com>
2025-09-13 23:04:50 +0300

    link added

---
+++ components/section/Cta.vue
@@ -0,0 +1,79 @@
+<script setup></script>
+
+<template>
+  <LayoutScrollSmooth>
+    <section id="contact" class="max-w-5xl mx-auto px-4 lg:px-0">
+      <div
+        class="mesh-gradient flex items-center flex-col rounded-3xl border bg-[#0b061a] border-white/10 p-10 relative"
+      >
+        <div class="flex flex-col items-center">
+          <h2 class="text-center !max-w-xl section-title">{{ CTA.label }}</h2>
+          <p class="paragraph max-w-xl text-center mt-8">
+            {{ CTA.description }}
+          </p>
+
+          <!-- Social Icons -->
+          <div class="flex gap-6 justify-center items-center mt-10">
+            <a
+              v-for="(item, i) in HERO.socialIcons"
+              :key="i"
+              :href="item.url"
+              target="_blank"
+              class="icon-wrapper p-4 bg-white rounded-full cursor-pointer transform transition-all duration-300 hover:scale-125 hover:rotate-12 hover:bg-[#3b82f6] hover:shadow-lg"
+            >
+              <img :src="item.icon" class="w-6 h-6" />
+            </a>
+          </div>
+
+          <Button
+            class="mt-10"
+            variant="btn-primary"
+            label="Start Securing"
+            disabled
+          />
+          
+
+          <div
+            class="my-8 w-4/5 h-[1px] bg-gradient-to-r from-white/0 via-white/20 to-white/0"
+          ></div>
+
+          <div class="flex items-center gap-4 lg:gap-10">
+            <NuxtLink
+              class="text-white/60 group text-sm lg:text-base"
+              v-for="link in NAVLINKS"
+              :to="link.url"
+            >
+              <div class="relative overflow-hidden">
+                <p
+                  class="group-hover:-translate-y-7 duration-500 ease-[cubic-bezier(0.19,1,0.22,1)]"
+                >
+                  {{ link.label }}
+                </p>
+                <p
+                  class="absolute top-7 left-0 group-hover:top-0 duration-500 ease-[cubic-bezier(0.19,1,0.22,1)]"
+                >
+                  {{ link.label }}
+                </p>
+              </div>
+            </NuxtLink>
+          </div>
+        </div>
+
+        <p class="text-sm text-white/30 text-center mt-8">
+          &copy; Copyright
+          <NuxtLink to="htt" target="_blank"
+            >Genet Shibru Built with ❤️
+          </NuxtLink>
+          2025
+        </p>
+
+        <div
+          class="absolute bottom-0 w-60 h-[1px] z-10 bg-gradient-to-r from-[#0b50e5]/0 via-[#0b50e5] to-[#0b50e5]/0"
+        ></div>
+        <div
+          class="-z-10 absolute bottom-4 w-96 bg-[#0b50e5] rounded-full blur-2xl h-40 animate-pulse"
+        ></div>
+      </div>
+    </section>
+  </LayoutScrollSmooth>
+</template>
---
+++ utils/content.ts
@@ -0,0 +1,154 @@
+export const NAVLINKS = [
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "About",
+    url: "#about",
+  },
+  {
+    label: "Works",
+    url: "#work",
+  },
+  {
+    label: "Service",
+    url: "/#service",
+  },
+];
+
+export const PROFILE = {
+  firstname: "Genet ",
+  lastname: "Shibru",
+  get fullname() {
+    return `${this.firstname} ${this.lastname}`;
+  },
+  role: "Aspiring SOC Analyst",
+  avatar: "/images/profile.jpg",
+};
+
+export const HERO = {
+  badge: "Available for freelance",
+  headline: `👋Ahoy, I’m ${PROFILE.firstname}`,
+  subline:
+    "Aspiring SOC Analyst passionate about hunting threats, securing systems, and keeping the digital world safe",
+  socialIcons: [
+    { icon: "/images/loLofoTelegram.svg", url: "https://t.me/parahez" },
+    {
+      icon: "/images/IoLogoLinkedin.svg",
+      url: "https://www.linkedin.com/in/genet-shibru-eshetu/",
+    },
+    {
+      icon: "/images/IoLogoGithub.svg",
+      url: "https://github.com/BinaryButterfly0",
+    },
+    {
+      icon: "/images/IoLogoTwitter.svg",
+      url: "https://x.com/shibru_gen49167?s=21",
+    },
+    
+    
+  ],
+  resumeButton: {
+    label: "Download My Resume",
+    href: "/Genet Shibru CV April.pdf",
+    download: true,
+    variant: "btn-dark",
+  },
+  
+};
+export const SERVICE = {
+  label: "service",
+  headline: "Security-focused solutions for your requirements.",
+  subline:
+    "I provide expertise in SOC operations, IT support, and operations management to help you secure and optimize your digital environment.",
+  services: [
+    {
+      name: "Security Operations Center (SOC)",
+      icon: "Scroll", // swap with your preferred icon or SVG
+      description:
+        "Monitoring, detecting, and responding to cyber threats to keep your organization secure and resilient.",
+      thumbnails: "",
+    },
+    {
+      name: "IT Support / Help Desk",
+      icon: "Headphones", // support/help-related icon
+      description:
+        "Providing reliable IT support and troubleshooting to ensure smooth and efficient operations.",
+      thumbnails: "",
+    },
+    {
+      name: "Operations Management",
+      icon: "Activity", // workflow/management-style icon
+      description:
+        "Optimizing workflows, managing processes, and improving efficiency across your organization.",
+      thumbnails: "",
+    },
+  ],
+};
+
+
+
+
+
+
+
+
+
+export const WORK = {
+  label: "work",
+  headline: "Custom design solutions for your requirements.",
+  subline:
+    "I specialize in crafting user-centered solutions for businesses and individuals. Let’s create something extraordinary together.",
+  works: [
+    {
+      name: "SOC Log Analysis with Splunk",
+      live_demo: "https://github.com/BinaryButterfly0/SOC-Log-Analysis-with-Splunk",
+      description:
+        "This project focuses on log analysis and threat detection using Splunk, a leading platform for searching, monitoring, and analyzing machine-generated data. The logs are systematically categorized based on server types—including Windows, Linux, and Web servers—and protocols such as DNS, HTTP, SSH, and Zeek. By organizing logs in this way, the project enables efficient monitoring, correlation of events, and rapid detection of anomalies or security incidents across diverse IT environments. This structured approach helps security analysts gain actionable insights and maintain robust system security.",
+      thumbnails: "LA.png",
+      type: "Home Lab",
+    },
+    {
+      name: "Network Analysis with Wireshark",
+      live_demo: "#",
+      description: "This lab focuses on network analysis using Wireshark, with an emphasis on TLS (Transport Layer Security) traffic. Students will explore how TLS secures data over the network, understand the handshake process, and examine key metadata such as server names, certificate details, TLS versions, and cipher suites. By analyzing TLS traffic, students gain practical skills in identifying outdated protocols, suspicious certificates, and potential misuse of encrypted channels, enhancing their ability to monitor and secure network communications effectively.",
+      thumbnails: "NA.png",
+      type: "Home Lab",
+    },
+    {
+      name: "Incident Response",
+      live_demo: "#",
+      description:
+        "This lab focuses on incident detection, analysis, and response in a Windows environment. It covers the incident response lifecycle—from identifying suspicious activity and detecting threats, to containing, eradicating, and recovering from incidents. Detection involves monitoring logs and alerts for anomalies such as failed logins, malware execution, or unauthorized access. Threats are analyzed by correlating events, identifying attacker behavior, and determining the scope of compromise. All findings are logged and documented to support reporting, forensic analysis, and improving security measures.",
+      thumbnails: "IR.png",
+      type: "Home Lab",
+    },
+    {
+      name: "Coming Soon",
+      live_demo: "#",
+      description:
+        "Coming Soon",
+      thumbnails: "SEIM.png",
+      type: "Home Lab",
+    },
+    
+  ],
+};
+
+export const ABOUT = {
+  label: "Let me introduce my self",
+  introduce: [
+    `My name is ${PROFILE.fullname}, I work in IT and I’m deeply passionate about cybersecurity, with a special focus on building and exploring home labs 
+    around Security Operations Center (SOC) practices. I enjoy continuously learning, experimenting with security tools, and strengthening my skills to better understand 
+    threats and defenses.`,
+
+    "Always eager to collaborate and grow, I’m committed to building a strong path in cybersecurity!",
+  ],
+};
+
+export const CTA = {
+  label: "Let’s Secure Something Great Together",
+  description:
+    "Want to protect your systems and hunt down threats? I’m here to help you strengthen defenses, respond to incidents, and uncover hidden risks—efficient, proactive, and security-focused.",
+};
